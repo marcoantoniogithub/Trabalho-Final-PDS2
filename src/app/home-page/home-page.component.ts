6fd06ce90eed5a84d8b891280a0c46de54276373
Rogerio Miranda <rogercomp@gmail.com>
2020-05-19 12:18:28 -0300

    Ajustes Models

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -3,7 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProductService } from '../service/product.service';
-import { Product } from 'src/models/product';
+import { Product } from 'src/models/product.model';
 import { Observable } from 'rxjs';
 
 
